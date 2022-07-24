f97c9c444477e376df340988009c31494df27826
wei-dot <walton6180831@gmail.com>
2022-07-25 01:18:58 +0800

    修改白名單和key

--- app/app/settings.py
+++ app/app/settings.py
@@ -28,12 +28,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*0+me-vzkg2$s00ybm1wn5-bxf!^l-dxhmw2#=s3nr8b)pwbe)'
+SECRET_KEY = secret['django']['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
