3451807d0ae574d5bca080207a704c069d096eec
wtbigak <walton6180831@gmail.com>
2022-09-06 02:41:49 +0800

    沒用的weboscket廣播

--- app/api/consumers.py
+++ app/api/consumers.py
@@ -5,6 +5,7 @@ from api.models import chat_room_data
 from asgiref.sync import sync_to_async
 from channels.db import database_sync_to_async
 
+
 class StripConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         if self.scope["user"].is_anonymous:
@@ -90,27 +91,26 @@ class MessageConsumer(AsyncWebsocketConsumer):
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = "chat_%s" % self.room_name
 
         # Join room group
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
-
         await self.accept()
-       
+      
 
     async def disconnect(self, close_code):
         # Leave room group
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
-    
     # Receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
-
-      
+        await self.get_all_message()
+        await self.post()
         # save message to database
         await self.save_message(message)
         # Send message to room group
@@ -124,7 +124,21 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({"message": message}))
-    
+
     @sync_to_async
     def save_message(self, message):
         chat_room_data.objects.create(chat_room_name=self.room_name, message=message)
+
+    @sync_to_async
+    def get_all_message(self):
+        self.data = chat_room_data.objects.all()
+
+    @sync_to_async
+    def post(self):
+        for m in self.data:
+            data = {
+                "message": m.message,
+            }
+            self.channel_layer.group_send(
+            self.room_group_name, {"type": "chat_message", "message": data}
+        )
